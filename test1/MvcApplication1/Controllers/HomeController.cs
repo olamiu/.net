109603c1836c2554402ee8bd4df0f52df706134c
olamiu <amiu@onelegacy.org>
2015-07-09 18:21:05 -0700

    First commit

---
+++ test1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "OneLegacy";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "OneLegacy.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "OneLegacy contact page.";
+
+            return View();
+        }
+    }
+}
